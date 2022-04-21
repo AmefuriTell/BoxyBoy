5ca7ac79087bb21d7601652e229ef599270d30da
AmefuriTell <higurashikamakiri@gmail.com>
2022-04-21 22:35:09 +0900

    入力時のメッセージを追加

--- Entity.cpp
+++ Entity.cpp
@@ -4,8 +4,11 @@ Entity::Entity(){}
 
 void Entity::input()
 {
+    std::cerr << "箱の数を入力してください" << std::endl;
     std::cin >> boxNum;
     box.resize(boxNum);
+    std::cerr << "箱の座標を入力してください" << std::endl;
     for(auto &in : box)std::cin >> in.first >> in.second;
+    std::cerr << "Playerの座標を入力してください" << std::endl;
     std::cin >> player.first >> player.second;
 }
--- Stage.cpp
+++ Stage.cpp
@@ -4,10 +4,14 @@ Stage::Stage(){}
 
 void Stage::input()
 {
+    std::cerr << "マップの大きさはなんですか？x yの順で入力してください" << std::endl;
     std::cin >> sizeX >> sizeY;
     map.resize(sizeY);
+    std::cerr << "mapチップを入力してください" << std::endl;
     for (auto in : map)std::cin >> in;
 
+    std::cerr << "箱の数を入力してください" << std::endl;
     std::cin >> boxNum;
+    std::cerr << "ゴールの座標を入力してください" << std::endl;
     for(auto in : goal)std::cin >> in.first >> in.second;
 }
